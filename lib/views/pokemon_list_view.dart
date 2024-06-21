c0d7d140fd402f3f45cdae9c49c1de58af5c4bde
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-06-21 17:34:39 +0700

    adjustment on the card list

--- lib/views/pokemon_list_view.dart
+++ lib/views/pokemon_list_view.dart
@@ -18,51 +18,57 @@ class PokemonListView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pokémon List'),
       ),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: (ScrollNotification scrollInfo) {
-          if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-            // Load more data when the end of the list is reached
-            pokemonController.loadMorePokemon();
-          }
-          return false;
-        },
-        child: pokemonController.isLoading &&
-                pokemonController.pokemonList.isEmpty
-            ? const Center(child: CircularProgressIndicator())
-            : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: crossAxisCount, // Number of columns
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                    childAspectRatio:
-                        3 / 2, // Adjust the aspect ratio as needed
-                  ),
-                  itemCount: pokemonController.pokemonList.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index == pokemonController.pokemonList.length) {
-                      return pokemonController.isLoading
-                          ? const Center(child: CircularProgressIndicator())
-                          : const SizedBox();
-                    }
-                    final pokemon = pokemonController.pokemonList[index];
-                    return PokemonListItem(
-                      pokemon: pokemon,
-                      onTap: () async {
-                        await pokemonController.loadPokemonDetail(pokemon.url);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                PokemonDetailView(pokemon: pokemon),
-                          ),
-                        );
-                      },
-                    );
-                  },
+      body: Stack(
+        children: [
+          NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification scrollInfo) {
+              if (scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent &&
+                  !pokemonController.isLoading) {
+                // Load more data when the end of the list is reached
+                pokemonController.loadMorePokemon();
+              }
+              return false;
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount, // Number of columns
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 3 / 2, // Adjust the aspect ratio as needed
                 ),
+                itemCount: pokemonController.pokemonList.length,
+                itemBuilder: (context, index) {
+                  final pokemon = pokemonController.pokemonList[index];
+                  return PokemonListItem(
+                    pokemon: pokemon,
+                    onTap: () async {
+                      await pokemonController.loadPokemonDetail(pokemon.url);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              PokemonDetailView(pokemon: pokemon),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
+            ),
+          ),
+          if (pokemonController.isLoading)
+            const Positioned(
+              bottom: 16,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
--- lib/views/widgets/pokemon_list_item.dart
+++ lib/views/widgets/pokemon_list_item.dart
@@ -15,29 +15,40 @@ class PokemonListItem extends StatelessWidget {
       child: Card(
         elevation: 2,
         margin: const EdgeInsets.all(4),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: pokemon.imageUrl.isNotEmpty
-                        ? NetworkImage(pokemon.imageUrl)
-                        : const AssetImage('assets/images/image_not_found.png')
-                            as ImageProvider,
-                    fit: BoxFit.contain,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16), // Add border radius here
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: 5.0), // Add top and bottom padding to the Column
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(16)),
+                    image: DecorationImage(
+                      image: pokemon.imageUrl.isNotEmpty
+                          ? NetworkImage(pokemon.imageUrl)
+                          : const AssetImage(
+                                  'assets/images/image_not_found.png')
+                              as ImageProvider,
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              pokemon.name,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              const SizedBox(height: 8),
+              Text(
+                pokemon.name,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );

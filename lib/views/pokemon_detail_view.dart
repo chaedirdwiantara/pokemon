ae46c72c3409e493cd2ec27219c84a0bf3db87c9
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-06-23 06:20:57 +0700

    add shared preference with like feature

--- lib/views/pokemon_detail_view.dart
+++ lib/views/pokemon_detail_view.dart
@@ -3,23 +3,63 @@ import 'package:pokemon_app/models/pokemon.dart';
 import 'widgets/pokemon_detail_top.dart';
 import 'widgets/pokemon_detail_bottom.dart';
 import 'widgets/pokemon_detail_image_text.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class PokemonDetailView extends StatelessWidget {
+class PokemonDetailView extends StatefulWidget {
   final Pokemon pokemon;
 
   const PokemonDetailView({required this.pokemon});
+
+  @override
+  _PokemonDetailViewState createState() => _PokemonDetailViewState();
+}
+
+class _PokemonDetailViewState extends State<PokemonDetailView> {
+  late bool isLiked;
+  late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    isLiked = false;
+    _loadLikeStatus();
+  }
+
+  Future<void> _loadLikeStatus() async {
+    prefs = await SharedPreferences.getInstance();
+    List<String>? likedPokemonIds = prefs.getStringList('likedPokemonIds');
+    if (likedPokemonIds != null) {
+      setState(() {
+        isLiked = likedPokemonIds.contains(widget.pokemon.id.toString());
+      });
+    }
+  }
+
+  Future<void> _toggleLike() async {
+    List<String>? likedPokemonIds =
+        prefs.getStringList('likedPokemonIds') ?? [];
+    if (isLiked) {
+      likedPokemonIds.remove(widget.pokemon.id.toString());
+    } else {
+      likedPokemonIds.add(widget.pokemon.id.toString());
+    }
+    await prefs.setStringList('likedPokemonIds', likedPokemonIds);
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          PokemonDetailTop(pokemon: pokemon),
+          PokemonDetailTop(pokemon: widget.pokemon),
           Align(
             alignment: Alignment.bottomCenter,
-            child: PokemonDetailBottom(pokemon: pokemon),
+            child: PokemonDetailBottom(pokemon: widget.pokemon),
           ),
-          PokemonDetailImageText(pokemon: pokemon),
+          PokemonDetailImageText(pokemon: widget.pokemon),
           Positioned(
             top: 40,
             left: 0,
@@ -28,6 +68,18 @@ class PokemonDetailView extends StatelessWidget {
               onPressed: () {
                 Navigator.pop(context);
               },
+            ),
+          ),
+          Positioned(
+            top: 40,
+            right: 6,
+            child: IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: isLiked ? Colors.pink : Colors.white,
+                size: 26.0,
+              ),
+              onPressed: _toggleLike,
             ),
           ),
         ],
